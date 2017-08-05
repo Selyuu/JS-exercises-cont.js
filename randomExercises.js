34156be714c8b461dfe63ed53a032d3cb8e7dd61
Selyuu <rainierlouis@gmail.com>
2017-08-05 19:30:02 +0200

    updated exercises

--- randomExercises.js
+++ randomExercises.js
@@ -212,6 +212,53 @@ var sums = arrays.reduce((acc, val, i) => {
 
 console.log(sums)
 
+// Students FP
+const students = [
+  {
+    name: "Anna",
+    sex: "f",
+    grades: [4.5, 3.5, 4]
+  },
+  {
+    name: "Dennis",
+    sex: "m",
+    country: "Germany",
+    grades: [5, 1.5, 4]
+  },
+  {
+    name: "Martha",
+    sex: "f",
+    grades: [5, 4, 2.5, 3]
+  },
+  {
+    name: "Brock",
+    sex: "m",
+    grades: [4, 3, 2]
+  }
+]
+
+// Filter female students
+
+
+
+// Compute female student results
+const femaleStudentsResults = [];
+for (const student of students) {
+  if (student.sex === "f") {
+    let gradesSum = 0;
+    for (const grade of student.grades) {
+      gradesSum += grade;
+    }
+    const averageGrade = gradesSum / student.grades.length;
+    femaleStudentsResults.push({
+      name: student.name,
+      avgGrade: averageGrade
+    })
+  }
+}
+
+console.log(femaleStudentsResults)
+
 
 
 
